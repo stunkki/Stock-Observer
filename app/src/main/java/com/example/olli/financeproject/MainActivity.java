79c16af058f2fba2b0ad145c6bd942650ac80e55
Olli Saari <saariollip@gmail.com>
2016-08-30 19:34:14 +0300

    Toolbar and user interface

--- app/src/main/java/com/example/olli/financeproject/MainActivity.java
+++ app/src/main/java/com/example/olli/financeproject/MainActivity.java
@@ -11,16 +11,19 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.http.util.ByteArrayBuffer;
 
-import android.app.Activity;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.*;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     EditText setSymbol;
     TextView symbolHeader;
@@ -31,12 +34,19 @@ public class MainActivity extends Activity {
     TextView changePercentageOut;
     Button getQuote;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         setSymbol = (EditText) findViewById(R.id.setSymbol);
+        symbolHeader = (TextView) findViewById(R.id.symbolHeader);
+        priceHeader = (TextView) findViewById(R.id.priceHeader);
+        changePercentageHeader = (TextView) findViewById(R.id.changePercentageHeader);
         symbolOut = (TextView) findViewById(R.id.stockSymbolOutput);
         priceOut = (TextView) findViewById(R.id.stockPriceOutput);
         changePercentageOut = (TextView) findViewById(R.id.stockChangePercentageOutput);
@@ -124,6 +134,55 @@ public class MainActivity extends Activity {
             }
         }
     }
+
+    // BEGIN_INCLUDE(create_menu)
+    /**
+     * Use this method to instantiate your menu, and add your items to it. You
+     * should return true if you have added items to it and want the menu to be displayed.
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate our menu from the resources by using the menu inflater.
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        // It is also possible add items here. Use a generated id from
+        // resources (ids.xml) to ensure that all menu ids are distinct.
+        MenuItem locationItem = menu.add(0, R.id.menu_location, 0, R.string.menu_location);
+        locationItem.setIcon(R.drawable.ic_location);
+
+        // Need to use MenuItemCompat methods to call any action item related methods
+        MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
+
+        return true;
+    }
+    // END_INCLUDE(create_menu)
+
+    // BEGIN_INCLUDE(menu_item_selected)
+    /**
+     * This method is called when one of the menu items to selected. These items
+     * can be on the Action Bar, the overflow menu, or the standard options menu. You
+     * should return true if you handle the selection.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                // Here we might start a background refresh task
+
+                return true;
+            /*
+            case R.id.menu_location:
+                // Here we might call LocationManager.requestLocationUpdates()
+                return true;
+            */
+            case R.id.menu_settings:
+                // Here we would open up our settings activity
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    // END_INCLUDE(menu_item_selected)
 }
 
 
