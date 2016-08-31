371461cfef5770f8a506924dda6ebd5361aebb3b
Olli Saari <saariollip@gmail.com>
2016-08-31 11:56:51 +0300

    ActionBar changed to Toolbar for flexibility

--- app/src/main/java/com/example/olli/financeproject/MainActivity.java
+++ app/src/main/java/com/example/olli/financeproject/MainActivity.java
@@ -12,15 +12,17 @@ import java.util.concurrent.ExecutionException;
 import org.apache.http.util.ByteArrayBuffer;
 
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.*;
+import android.support.v7.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.widget.Toolbar;
+
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
     TextView changePercentageOut;
     Button getQuote;
 
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //Adding toolbar to Main view
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        initToolbar(toolbar);
+       // setSupportActionBar(toolbar);
 
         setSymbol = (EditText) findViewById(R.id.setSymbol);
         symbolHeader = (TextView) findViewById(R.id.symbolHeader);
@@ -135,54 +141,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // BEGIN_INCLUDE(create_menu)
-    /**
-     * Use this method to instantiate your menu, and add your items to it. You
-     * should return true if you have added items to it and want the menu to be displayed.
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate our menu from the resources by using the menu inflater.
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        // It is also possible add items here. Use a generated id from
-        // resources (ids.xml) to ensure that all menu ids are distinct.
-        MenuItem locationItem = menu.add(0, R.id.menu_location, 0, R.string.menu_location);
-        locationItem.setIcon(R.drawable.ic_location);
-
-        // Need to use MenuItemCompat methods to call any action item related methods
-        MenuItemCompat.setShowAsAction(locationItem, MenuItem.SHOW_AS_ACTION_IF_ROOM);
-
-        return true;
+    public void initToolbar(Toolbar toolbar) {
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.searchItem:
+                        Toast.makeText(getApplicationContext(), "Search", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.filterItem:
+                        Toast.makeText(getApplicationContext(), "Filter", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+        // Inflate a menu to be displayed in the toolbar
+        toolbar.inflateMenu(R.menu.toolbar_menu);
     }
-    // END_INCLUDE(create_menu)
 
-    // BEGIN_INCLUDE(menu_item_selected)
-    /**
-     * This method is called when one of the menu items to selected. These items
-     * can be on the Action Bar, the overflow menu, or the standard options menu. You
-     * should return true if you handle the selection.
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_refresh:
-                // Here we might start a background refresh task
-
-                return true;
-            /*
-            case R.id.menu_location:
-                // Here we might call LocationManager.requestLocationUpdates()
-                return true;
-            */
-            case R.id.menu_settings:
-                // Here we would open up our settings activity
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    // END_INCLUDE(menu_item_selected)
 }
 
 
